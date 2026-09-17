bd580b3dbabed48793927163c0675ac592785272
mdikram503-alt <mdikram503@gmail.com>
2026-09-18 00:31:43 +0400

    Fix sqlite shared memory

--- database.py
+++ database.py
@@ -25,7 +25,7 @@ def get_db():
             conn = sqlite3.connect(DB_PATH)
         except OSError:
             print("[WARNING] Read-only file system detected. Using in-memory sqlite.")
-            conn = sqlite3.connect(":memory:")
+            conn = sqlite3.connect("file::memory:?cache=shared", uri=True)
         conn.row_factory = sqlite3.Row
         return conn
     
