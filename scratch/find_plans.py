bb47cf320838ce86d16ef1d2e65fba88bb8cce9e
mdikram503-alt <mdikram503@gmail.com>
2026-09-24 19:10:32 -0700

    Remove unused cleanup file: scratch/find_plans.py

--- scratch/find_plans.py
+++
@@ -1,6 +0,0 @@
-import re
-
-content = open('public/index.html', encoding='utf-8').read()
-matches = re.findall(r'id:"[^"]+",title:"[^"]+",flag:"[^"]+",data:"[^"]+"[^\}]+', content)
-for m in matches[:5]:
-    print(m)
